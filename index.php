7b1b5ab739a8dfb859d745b786050915b6f61682
iamtheluiz <iamtheluiz.dev@gmail.com>
2019-01-11 20:32:36 -0200

    Adicionado materialize e jquery

--- index.php
+++ index.php
@@ -6,9 +6,13 @@
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Gestor de Armários | Login</title>
+        <link rel="stylesheet" href="/css/materialize.css">
+        <link rel="stylesheet" href="/css/style.css">
         <link rel="manifest" href="/manifest.json">
     </head>
     <body>
 
+        <script type="application/javascript" src="/js/jquery-3.3.1.min.js"></script>
+        <script type="application/javascript" src="/js/materialize.js"></script>
     </body>
 </html>
