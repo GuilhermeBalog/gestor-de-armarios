bbf0ccf0714c736da5bb88669e0925d2470ec7f8
GuilhermeBalog <guibalog@gmail.com>
2019-01-12 21:22:22 -0200

    Validação extra acrescentada

--- js/menu_li_active.js
+++ js/menu_li_active.js
@@ -1,6 +1,7 @@
 function convert_title(str)
 {
     str = str.replace("Gestor de Armários | ", "").toLowerCase();
+    str = str.replace('á', 'a');
     str = str.split(" ");
     str = str.join("_");
     return "li_"+str;
