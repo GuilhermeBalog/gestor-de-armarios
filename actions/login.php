61752dd8119240711b82badada9944d6bd025d76
iamtheluiz <iamtheluiz.dev@gmail.com>
2019-01-12 16:08:22 -0200

    Removidos comentários dos redirecionamentos do login.php

--- actions/login.php
+++ actions/login.php
@@ -10,15 +10,15 @@ if(isset($_POST["tx_login"]) and isset($_POST["tx_pass"])){
 
         //Verifica se foi executado
         if($login == true){
-            //$admin->redirect("/home.php");
+            $admin->redirect("/home.php");
         }else{
-            //$admin->redirect("/index.php");
+            $admin->redirect("/index.php");
         }
     }else{
         $admin->alert("É necessário preencher o formulário de acesso!");
-        //$admin->redirect("../index.php");
+        $admin->redirect("../index.php");
     }
 }else{
     $admin->alert("Envie o formulário de login para acessar essa página!");
-    //$admin->redirect("../index.php");
+    $admin->redirect("../index.php");
 }
