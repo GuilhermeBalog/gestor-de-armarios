5868c45782fde81752b5454354a25b3b39f32b41
iamtheluiz <iamtheluiz.dev@gmail.com>
2019-01-13 15:06:34 -0200

    Retirado console.log da função para edição

--- js/editar_dados.js
+++ js/editar_dados.js
@@ -6,7 +6,6 @@ function editar_dados(tabela, codigo, lista){
 
     //Pega os outros dados
     for(var i = 0; i <= lista.length - 1; i++){
-        console.log(i);
         editar = document.getElementById("editar_"+lista[i]);
         lista[i] = lista[i].replace("_","-");
         atributo = elemento.getAttribute("data-"+lista[i]);
